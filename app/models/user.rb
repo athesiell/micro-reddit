b66cf81ae3cc6ddf5e4946428a4d713f80cd3423
Aidar <ar.ibraev98@gmail.com>
2024-01-29 20:35:57 +0600

    Add comment model and associations between other models, migrate db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
 
   validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
   validates :password, presence: true, length: { in: 6..20 }
